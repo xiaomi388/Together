a846505e11334f2e3bd83bb132288c9c17eb667b
xiaomi388 <xiaomi388@gmail.com>
2019-08-27 00:00:36 +0800

    Bugs fixed

--- src/components/ChatWidget.js
+++ src/components/ChatWidget.js
@@ -89,12 +89,12 @@ class ChatWidget extends React.Component {
         <div className="cameraArea">
           <div className="remoteCameraArea">
             <Input value={this.state.remoteUserName} disabled />
-            <video autoPlay id="remoteVideo" ref={video => (this.remoteVideo = video)}></video>
+            <video autoPlay playsInline id="remoteVideo" ref={video => (this.remoteVideo = video)}></video>
           </div>
           <div className="localCameraArea">
             <Input value={this.state.localUserName} onChange={ e => this.handleNameInput(e) } />
             <Button className="toggleCamera" shape="circle" type="primary" icon="video-camera" onClick={this.props.toggleCamera} ></Button>
-            <video autoPlay id="localVideo" muted ref={video => (this.localVideo = video)}></video>
+            <video autoPlay playsInline id="localVideo" muted ref={video => (this.localVideo = video)}></video>
           </div>
         </div>
         <p style={{ textAlign: 'right', marginRight: '5%' }}> Current Status: {this.props.currentState} </p>
